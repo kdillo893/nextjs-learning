e62a344687c1de3afac3ffaa23d8e51e410d00ed
Kevin Dillon <kdillo893@gmail.com>
2023-11-02 21:16:56 -0500

    add the rest of the data to the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,17 @@ import { Card } from "../ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import { lusitana } from "../ui/fonts";
-import { fetchRevenue } from "../lib/data";
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
-  console.log(revenue);
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers
+  } = await fetchCardData();
 
     return (
     <main>
@@ -14,18 +20,18 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        { <Card title="Collected" value={totalPaidInvoices} type="collected" /> }
+        { <Card title="Pending" value={totalPendingInvoices} type="pending" /> }
+        { <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> }
+        { <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        /> }
       </div>
       <div>
         { <RevenueChart revenue={revenue}  /> }
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        { <LatestInvoices latestInvoices={latestInvoices} /> }
       </div>
     </main>
     )
